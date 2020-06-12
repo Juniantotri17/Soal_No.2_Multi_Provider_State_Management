a6f0d023dff5ba28e9ba88f1d297c2fd2c3985a4
Juniantotri17 <juniantotri17@gmail.com>
2020-06-12 18:12:53 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:multiprovider/cart.dart';
+import 'package:provider/provider.dart';
+import 'package:multiprovider/money.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Money>(
+            create: (context) => Money(),
+          ),
+          ChangeNotifierProvider<Cart>(
+            create: (context) => Cart(),
+          )
+        ],
+        child: Scaffold(
+          floatingActionButton: Consumer<Money>(
+            builder: (context, money, _) => Consumer<Cart>(
+              builder: (context, cart, _) => FloatingActionButton(
+                onPressed: () {
+                  if (money.balance >= 20000) {
+                    cart.quantity += 1;
+                    money.balance -= 20000;
+                  }
+                },
+                child: Icon(Icons.add_shopping_cart),
+                backgroundColor: Colors.yellow,
+              ),
+            ),
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: Center (
+              child: Text("Multi Provider 20160801250"),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Balance"),
+                    Container(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Consumer<Money>(
+                          builder: (context, money, _) => Text(
+                            money.balance.toString(),
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                      height: 30,
+                      width: 150,
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.yellow[100],
+                          border: Border.all(color: Colors.purple, width: 2)),
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Consumer<Cart>(
+                      builder: (context, cart, _) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Mangga (20000) x " + cart.quantity.toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            (20000 * cart.quantity).toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  height: 30,
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(color: Colors.black, width: 2)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
